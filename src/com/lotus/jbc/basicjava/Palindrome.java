9f7a231ad63e5c07f4993ea38e6ae047c6480c68
jan-yatco <jan-yatco@users.noreply.github.com>
2020-02-21 21:47:16 +0800

    Exercise3WithAssignment
    Palindrome and OddEven Comparator assignment

--- src/com/lotus/jbc/basicjava/Palindrome.java
+++ src/com/lotus/jbc/basicjava/Palindrome.java
@@ -1,5 +1,7 @@
 package com.lotus.jbc.basicjava;
 
+import java.util.Arrays;
+
 public class Palindrome {
 	
 	/**
@@ -12,8 +14,29 @@ public class Palindrome {
 	 * 		false - if the array is not a palindrome.
 	 */
 	public boolean determinePalindrome(char[] chars) {
-		
 		// Modify content of this method
-		return false;
+		Boolean isPalindrome = false;
+		int ctrA;
+		int ctrB = chars.length - 1;
+		char[] charsCompare = new char[chars.length];
+
+		// Store chars in reverse order to charsCompare
+		for (ctrA = 0; ctrA < chars.length; ctrA++) {
+			charsCompare[ctrB] = chars[ctrA];
+			ctrB--;
+		}
+
+		// Compares both arrays if equal
+		for (ctrA = 0; ctrA < chars.length; ctrA++) {
+			if (charsCompare[ctrA] == chars[ctrA]) {
+				isPalindrome = true;
+			} else {
+				isPalindrome = false;
+				break;
+			}
+		}
+
+		// Return value of isPalindrome
+		return isPalindrome;
 	}
 }
